21d46ec6221c14360cb877755424446159235455
Alexandre Marcelino <amsantos9@gmail.com>
2022-06-09 06:33:06 -0300

    Criado repository e service para Categoria

--- backend/src/main/java/com/alexandre/cursomc/resources/CategoriaResource.java
+++ backend/src/main/java/com/alexandre/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,27 @@
 package com.alexandre.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexandre.cursomc.domain.Categoria;
+import com.alexandre.cursomc.resources.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 =  new Categoria(2, "Escritório");
-		
-		List<Categoria> list = new ArrayList();
-		list.add(cat1);
-		list.add(cat2);
-		
-		
-		return list;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {				
+		Optional<Categoria> obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ backend/src/main/java/com/alexandre/cursomc/resources/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.alexandre.cursomc.resources.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandre.cursomc.domain.Categoria;
+import com.alexandre.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Optional<Categoria> find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj;
+	}	
+}
