2f0aae6a85f046d1d97bbfbe64c02187661ca2ce
Alexandre Marcelino <amsantos9@gmail.com>
2022-06-15 06:32:46 -0300

    Item Pedido, ItemPedidoPK e README

---
+++ backend/src/main/java/com/alexandre/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.alexandre.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alexandre.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
