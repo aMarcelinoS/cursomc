b9cbc9ec7c4debfe8c1c6cc2a6f2ea54b793c427
Alexandre Marcelino <amsantos9@gmail.com>
2022-06-24 12:28:08 -0300

    Inserindo um novo Cliente com POST

--- backend/src/main/java/com/alexandre/cursomc/resources/ClienteResource.java
+++ backend/src/main/java/com/alexandre/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.alexandre.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alexandre.cursomc.domain.Cliente;
 import com.alexandre.cursomc.dto.ClienteDTO;
+import com.alexandre.cursomc.dto.ClienteNewDTO;
 import com.alexandre.cursomc.services.ClienteService;
 
 @RestController
@@ -51,6 +55,14 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> insert(@RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value = "/{id}")
--- backend/src/main/java/com/alexandre/cursomc/services/ClienteService.java
+++ backend/src/main/java/com/alexandre/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.alexandre.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.alexandre.cursomc.domain.Cidade;
 import com.alexandre.cursomc.domain.Cliente;
+import com.alexandre.cursomc.domain.Endereco;
+import com.alexandre.cursomc.domain.enums.TipoCliente;
 import com.alexandre.cursomc.dto.ClienteDTO;
+import com.alexandre.cursomc.dto.ClienteNewDTO;
 import com.alexandre.cursomc.repositories.ClienteRepository;
+import com.alexandre.cursomc.repositories.EnderecoRepository;
 import com.alexandre.cursomc.services.exceptions.DataIntegrityException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +28,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	// Busca um cliente por id no banco de dados
 	public Cliente find(Integer id) {
@@ -46,6 +56,16 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());		
 	}
+	
+	//Insere um novo cliente no BD
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = clienteRepository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+		
+	}
 
 	// Deleta um cliente cadastrado pelo id
 	public void delete(Integer id) {
@@ -63,7 +83,23 @@ public class ClienteService {
 		return clienteRepository.findAll(pageRequest);
 	}
 
+	//Instancia o Cliente através do seu DTO
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cidade = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getComplemento(), objDto.getNumero(), objDto.getBairro(), objDto.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(end);
+		cliente.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cliente.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cliente.getTelefones().add(objDto.getTelefone3());
+		}
+		return cliente;
 	}
 }
